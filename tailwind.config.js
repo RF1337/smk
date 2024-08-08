430dbbb2ec1608ff52c2691cff2f8fcdec4ec199
rf1337 <rasmus.ferst@gmail.com>
2024-08-08 09:18:14 +0200

    Added custom font and backup fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,48 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'basic': '#323332',
+      'second-page': '#093017',
+      'third-page': '#6F9AAA',
+      'fourth-page': '#01417F',
+      'fifth-page': '#63AB9E',
+      'sixth-page': '#7BC4C4',
+      'seventh-page': '#155058',
+      'eigth-page': '#13575F',
+      'ninth-page': '#3D3C3C',
+      'tenth-page': '#A44243',
+      'eleventh-page': '#6F9AAA',
+      'custom-gray': '#959495',
+      },
+      fontFamily:{
+        headings:[
+          'Hill',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          'Oxygen-Sans',
+          'Ubuntu',
+          'Cantarell',
+          '"Helvetica Neue"',
+          'sans-serif'
+        ],
+        body:[
+          'Hill',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          'Oxygen-Sans',
+          'Ubuntu',
+          'Cantarell',
+          '"Helvetica Neue"',
+          'sans-serif'
+        ]
+      }
+    },
   },
   plugins: [],
 }
